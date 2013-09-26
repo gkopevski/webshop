71b1b69c892c1f784877dad87e2ae9b5629e99bc
gkopevski <gkopevski@gmail.com>
2013-09-26 08:11:41 +0200

    Test email sender

---
+++ site/src/main/java/com/mycompany/controller/account/TestEmailSender.java
@@ -0,0 +1,62 @@
+package com.mycompany.controller.account;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class TestEmailSender {
+
+	public static void main(String[] args) {
+		
+	      String to = "goran@medical-it-revolution.com";
+
+	      // Sender's email ID needs to be mentioned
+	      String from = "goran@medical-it-revolution.com";
+
+	      // Assuming you are sending email from localhost
+	      String host = "192.168.1.12";
+	      String port = "25";
+
+	      // Get system properties
+	      Properties properties = System.getProperties();
+
+	      // Setup mail server
+	      properties.setProperty("mail.smtp.host", host);
+	      properties.setProperty("mail.smtp.port", port);
+	      properties.setProperty("mail.debug", "true");
+
+	      // Get the default Session object.
+	      Session session = Session.getDefaultInstance(properties);
+
+	      try{
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO,
+	                                  new InternetAddress(to));
+
+	         // Set Subject: header field
+	         message.setSubject("This is the Subject Line!");
+
+	         // Now set the actual message
+	         message.setText("This is actual message");
+
+	         // Send message
+	         Transport.send(message);
+	         System.out.println("Sent message successfully....");
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	      }
+
+	}
+
+}
