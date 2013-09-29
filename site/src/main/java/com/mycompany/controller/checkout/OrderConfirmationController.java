43b4722b15fb46460bd37cfc97a6a4dce61e5d97
vojdan_ <vojdan_@192.168.1.100>
2013-09-29 16:05:08 +0200

    paypal send confirmation email

--- site/src/main/java/com/mycompany/controller/checkout/OrderConfirmationController.java
+++ site/src/main/java/com/mycompany/controller/checkout/OrderConfirmationController.java
@@ -16,6 +16,7 @@
 
 package com.mycompany.controller.checkout;
 
+import org.apache.velocity.app.VelocityEngine;
 import org.broadleafcommerce.common.email.service.EmailService;
 import org.broadleafcommerce.common.email.service.info.EmailInfo;
 import org.broadleafcommerce.core.order.domain.Order;
@@ -23,16 +24,22 @@ import org.broadleafcommerce.core.order.service.OrderService;
 import org.broadleafcommerce.core.web.controller.checkout.BroadleafOrderConfirmationController;
 import org.broadleafcommerce.profile.core.dao.CustomerDao;
 import org.broadleafcommerce.profile.core.domain.Customer;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.velocity.VelocityEngineUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class OrderConfirmationController extends BroadleafOrderConfirmationController {
@@ -48,25 +55,68 @@ public class OrderConfirmationController extends BroadleafOrderConfirmationContr
 
     @Resource(name="blOrderService")
     protected OrderService orderService;
+    
+    //VK
+    @Resource(name="mailSender")
+	 private JavaMailSender mailSender;
+	 
+	 @Resource(name="velocityEngine")
+	 private VelocityEngine velocityEngine;
 
     @RequestMapping(value = "/confirmation/{orderNumber}", method = RequestMethod.GET)
     public String displayOrderConfirmationByOrderNumber(@PathVariable("orderNumber") String orderNumber, Model model,
             HttpServletRequest request, HttpServletResponse response) {
-        sendConfirmationEmail(orderNumber);
-        return super.displayOrderConfirmationByOrderNumber(orderNumber, model, request, response);
+        
+    	
+    	
+    	
+    	try {
+			sendConfirmationEmail(orderNumber);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+    	
+    	
+    	
+    	
+    	return super.displayOrderConfirmationByOrderNumber(orderNumber, model, request, response);
     }
     
-    public void sendConfirmationEmail(String orderNumber){
+    public void sendConfirmationEmail(String orderNumber) throws Exception{
         Order order = orderService.findOrderByOrderNumber(orderNumber);
         Customer customer = customerDao.readCustomerByEmail(order.getEmailAddress());
+        
+        
+        
         if (customer != null){
             HashMap<String, Object> vars = new HashMap<String, Object>();
             vars.put("customer", customer);
             vars.put("orderNumber", orderNumber);
             vars.put("order", order);
             emailService.sendTemplateEmail(customer.getEmailAddress(), getOrderConfirmationEmailInfo(), vars);
+            
+            sendOrderConfirmationEmailMIR(customer);
         }
+        
+        
     }
+    
+    private void sendOrderConfirmationEmailMIR(final Customer customer) throws Exception {
+	      MimeMessagePreparator preparator = new MimeMessagePreparator() {
+	         public void prepare(MimeMessage mimeMessage) throws Exception {
+	            MimeMessageHelper message = new MimeMessageHelper(mimeMessage);
+	            message.setTo(customer.getEmailAddress());
+	            message.setFrom("gkopevski@gmail.com"); // could be parameterized...
+	            Map model = new HashMap();
+	            model.put("customer", customer);
+	            String text = VelocityEngineUtils.mergeTemplateIntoString(velocityEngine, "com/mycompany/api/service/email/order-confirmation.vm", model);
+	            message.setText(text, true);
+	         }
+	      };
+	      this.mailSender.send(preparator);
+	   }
 
     public CustomerDao getCustomerDao() {
         return customerDao;
